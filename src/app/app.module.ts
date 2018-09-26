0330d67a049bb14d2fd00ac9d007c59388e2b239
Pooja P <pooja.cp@gmail.com>
2018-09-26 12:01:28 +1000

    Add QuoteFromAuthor Component and rest service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { QuoteFromAuthorComponent } from './quote-from-author/quote-from-author.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteFromAuthorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/quote-from-author/quote-from-author.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-from-author',
+  templateUrl: './quote-from-
+  author.component.html',
+  styleUrls: ['./quote-from-author.component.css']
+})
+export class QuoteFromAuthorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
